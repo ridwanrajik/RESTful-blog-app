5929a61777a5a7b3e3b275195e821122aa921c68
Ridwan <ridwanraji4@gmail.com>
2021-09-15 15:12:36 +0100

    new and create route

--- app.js
+++ app.js
@@ -41,7 +41,38 @@ app.get('/index', function (req, res){
     });
 });
 
+// New Route
+app.get('/index/new', function(req, res){
+    res.render('new');
+});
 
+// CREATE ROUTE
+app.post("/index", function(req, res){
+    // create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new")
+        } else {
+            //then redirect to the index
+            res.redirect("/index");
+        }
+    });
+});
+
+// app.post('/index', function(req, res){
+//     Blog.create({
+//         title: 'Blog Post 1',
+//         image: 'https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+//         body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur',
+//         created: {type: Date, default: Date.now}
+//     }, function(err, post){
+//         if (err){
+//             console.log('error!')
+//         } else{
+//             res.render('index',)
+//         }
+//     })
+// })
 
 app.listen(process.env.port || 3000, function(){
     console.log('RESTful-blog-app server have started!');
